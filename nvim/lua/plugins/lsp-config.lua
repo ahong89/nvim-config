06c18ac24e2ccc7edfc3eb8caed299d7318da3ec
Andrew Hong <honga010807@gmail.com>
2025-01-07 17:47:14 -0500

    1/7 update

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,6 +1,6 @@
 return {
 	{
-		"williamboman/mason.nvim",
+		"williamboman/mason.nvim", -- manage installation of lsps
 		config = function()
 			require("mason").setup()
 		end
@@ -18,9 +18,16 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require('lspconfig')
-			lspconfig.lua_ls.setup({})
+			vim.diagnostic.config({
+				signs = false
+			})
+			
 			vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+
+			lspconfig.lua_ls.setup({})
 		end
 	}
 }
