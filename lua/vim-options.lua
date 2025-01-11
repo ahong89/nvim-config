84c34b89971a37066cb3a997af3da1a82f9fe4d0
Andrew Hong <honga010807@gmail.com>
2025-01-11 17:46:48 -0500

    more changes?

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,8 +1,15 @@
+-- tab config
 vim.cmd("set noexpandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
+-- adds line numbers
 vim.cmd("set number")
 
+-- prevents enter from making new comments
+vim.cmd('autocmd BufEnter * set formatoptions-=cro')
+vim.cmd('autocmd BufEnter * setlocal formatoptions-=cro')
 
+-- stops swapfiles
+vim.opt.swapfile = false
